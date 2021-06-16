802bd129b1453b851581440c5fe5f768d2e2443a
Justin <justinmhoch@gmail.com>
2021-06-16 18:39:25 -0400

    Added blank pages and components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <img src={logo} alt=""></img>
+      <img src={logo} alt="" height="600" width="600"></img>
     </div>
   );
 }
