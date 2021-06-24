4198d5494c2f0e7c65c05f4416027af176fe89a5
Justin <justinmhoch@gmail.com>
2021-06-23 20:05:57 -0400

    Created rough draft to get certifications and watch providers

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -30,19 +30,57 @@ function MoviePage({ match }) {
     )
   } else {
     console.log(movieDetails);
-    const certs = movieDetails.release_dates.results.filter(cert => cert.iso_3166_1 === 'US');
 
-    console.log(certs);
-    console.log(certs[0].release_dates[0].certification);
-    // console.log(certs.release_dates.certification);
+    // Certification TODO: Refactor into a service
+    const getCerts = (details) => {
+      const certs = details.release_dates.results.filter(cert => cert.iso_3166_1 === 'US');
+      const cert = [];
+      if(certs[0]){
+        certs[0].release_dates.forEach(element => {
+          if(element.certification !== ""){
+            cert.push(element.certification);
+          };
+        });
+        if(cert.length > 0){
+          console.log(cert[0]); //Update to return the rating
+          return cert[0];
+        }else{
+          console.log("All certs are empty strings"); //Update to return false if no rating
+        };
+      }else{
+        console.log("No US certs available");
+      }
+    }
+    const cert = getCerts(movieDetails);
+
+    // Watch Providers TODO: Refactor into a service
+    const getWatchProviders = (details) => {
+      const watchProviders = details["watch/providers"].results;
+      if(watchProviders["US"]){
+        console.log(watchProviders["US"]); //Update to return array of watch providers
+      }else{
+        console.log("no watch providers");
+      };
+    }
+    getWatchProviders(movieDetails);
+
     return (
       <main className="bg-gray-600">
-        <div className="p-2">
-          <img className="rounded w-44" src={`http://image.tmdb.org/t/p/w342${movieDetails.poster_path}`} alt="" />
+        <div className="p-2 flex">
+          <img className="rounded w-44 mr-2" src={`http://image.tmdb.org/t/p/w342${movieDetails.poster_path}`} alt="" />
           <div>
-            <h2>{movieDetails.title}</h2>
+            <h2 className="text-xl font-bold text-white">{movieDetails.title}</h2>
+            <p>Runtime: {movieDetails.runtime}min</p>
+            <p>Rating: {cert}</p>
           </div>
         </div>
+
+        <div className="p-2">
+          <h2 className="text-lg font-bold text-white">Overview</h2>
+          <p>{movieDetails.overview}</p>
+        </div>
+
+        {/* TODO: Add watch list */}
       </main>
     )
   }
