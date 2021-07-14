01d11896449e169ac8c963fdbb31212bab8d4f06
Justin <justinmhoch@gmail.com>
2021-07-14 10:36:34 -0400

    Flex aligned logo and site title

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,14 +10,16 @@ import SearchBar from './SearchBar'
 function Header() {
   return (
     <header className="bg-gray-800 p-4 h-24s">
-      <div className="inline-block">
-        <Link to="/">
-          <img className="inline-block mr-3 h-10" src={logo} alt="logo"/>
-        </Link>
+      <div className="flex items-center">
+        <div>
+          <Link to="/">
+            <img className="inline-block mr-3 h-14" src={logo} alt="logo"/>
+          </Link>
+        </div>
+        <h1 className="text-white text-4xl">
+          Film Search
+        </h1>
       </div>
-      <h1 className="text-white text-4xl inline-block">
-        Film Search
-      </h1>
       <SearchBar />
     </header>
   )
